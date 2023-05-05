5c47eaaa8bf398b0865ba399942c4b1a33485516
killgriff22 <kilgrif22033@gmail.com>
2023-05-04 23:31:53 -0400

    05/04/23 most recent commit, generating README

--- app.py
+++ app.py
@@ -23,10 +23,7 @@ else:
         KEY = f.read()
         f.close()
 Webhook_Link = "https://discord.com/api/webhooks/1100045482413273108/lPY3PHleNHR4U2HLH1m8FfIAXdpe6ENAJsH7gEazvEHbwIVGSujK18B8jss_kM1iSZlN"
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-def getCookie():
+def getCookie() -> str:#get the token and give the name attatched and the appropriate login panel
     cookie = request.cookies.get('token')
     with open("static/JS/Users.json", 'r') as UserDataBase:
         database = json.load(UserDataBase)
@@ -39,10 +36,40 @@ def getCookie():
     else: 
         name=""
         Login="""
-        <li><a href="/login">Login</a></li>
-        <li><a href="/signup">Sign Up</a></li>
+        <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+        <li class="nav-itme"><a class="nav-link" href="/signup">Sign Up</a></li>
         """
     return name,Login
+def getpass() -> str:#get the password of the user unencrypted
+    name,_ = getCookie()
+    with open("static/JS/Users.json", 'r') as UserDataBase:
+        database = json.load(UserDataBase)
+        users = database['users']
+        tokenlst= database['tokens'].keys()
+        tokens = database['tokens']
+        UserDataBase.close()
+    if name == "":
+        return None
+    else:
+        return cybrsec.x4x5.decrypt(KEY,users[name] if not type(users[name]) == dict else users[name]['password'])
+def getpref() -> dict|None:#get the user preferences like theme, font, and font size 
+    with open("static/JS/UserPref.json", 'r') as UserprefDataBase:
+        name,_ = getCookie()
+        nopref=False
+        database = json.load(UserprefDataBase)
+        if name not in database.keys():
+            database.update({name:{
+                "theme":"dark",
+                "font":"Arial",
+                "fontsize":"12"
+            }})
+            nopref=True
+        userpref=database[name]
+        UserprefDataBase.close()
+    with open("static/JS/UserPref.json", 'w') as UserprefDataBase:
+                UserprefDataBase.write(json.dumps(database))
+                UserprefDataBase.close()
+    return userpref if userpref else None
 @app.route('/ex')
 def ex():
     name,login=getCookie()
@@ -116,7 +143,7 @@ def signup():
 @app.route('/welcome')
 def welcome():
     name,Login=getCookie()
-    return render_template('welcome.html', name="",login=Login)
+    return render_template('welcome.html', name=name,login=Login)
 @app.route('/chat',methods=['GET','POST'])
 def imgchat():
     name,Login=getCookie()
@@ -156,18 +183,81 @@ def imgchat():
     return render_template('imgchat.html',total=total,login=Login)
 @app.route('/about',methods=['GET','POST'])
 def aboutme():
+    panel=""
     if "logout" in request.args.keys():#logout
         resp = make_response("<meta http-equiv='refresh' content='0; url=/'>")
         resp.set_cookie('token', "")
-        return resp
+        return resp  
     if "usrprof" in request.args.keys():#user profile
-        if request.method == "GET":
-            name,Login=getCookie()
-            return render_template('about.html',login=Login,panel=f"""
-            <button onclick="updateProfile()">Update Profile</button>
-""")
+        name,Login=getCookie()
+        if request.method == "POST":
+            if "namechange" in request.form['type']:#change username
+                print("namechange")
+                name,_ = getCookie()
+                newname=request.form['newname']
+                with open("static/JS/Users.json", 'r') as UserDataBase:
+                    database = json.load(UserDataBase)
+                    users = database['users']
+                    tokens = database['tokens']
+                    UserDataBase.close()
+                if name in users.keys():
+                    users.update({newname: {"password":cybrsec.x4x5.encrypt(KEY,getpass()),"profimg":"https://cdn.discordapp.com/attachments/1100045482413273108/1100045482413273108/unknown.png"}})
+                    del users[name]
+                    tokens.update({random.randint(100000,999999): newname})
+                    del tokens[request.cookies.get('token')]
+                    payload = {'users': users, 'tokens': tokens}
+                    with open("static/JS/Users.json", 'w') as UserDataBase:
+                        UserDataBase.write(json.dumps(payload))
+                        UserDataBase.close()
+            if "passchange" in request.form['type']:#change password
+                name,_ = getCookie()
+                newpwd=request.form['newpwd']
+                with open("static/JS/Users.json", 'r') as UserDataBase:
+                    database = json.load(UserDataBase)
+                    users = database['users']
+                    tokenlst= database['tokens'].keys()
+                    tokens = database['tokens']
+                    UserDataBase.close()
+                if name in users.keys():
+                    newpass={name: {"password":cybrsec.x4x5.encrypt(KEY,newpwd), "profimg":"https://cdn.discordapp.com/attachments/1100045482413273108/1100045482413273108/unknown.png"}}
+                    users.update(newpass)
+                    payload = {'users': users, 'tokens': tokens}
+                    payload = {'users': users, 'tokens': tokens}
+                    with open("static/JS/Users.json", 'w') as UserDataBase:
+                        UserDataBase.write(json.dumps(payload))
+                        UserDataBase.close()
+        name,_=getCookie()
+        panel=f"""
+            <form action="" method="post" enctype="multipart/form-data">
+            <input hidden type="text" name="type" value="namechange"></input>
+            Username:<input type="text" name="newname" value="{name}" required></input>
+            <input type="submit" value="Change Name"></input>
+            </form><br>
+            <form action="" method="post" enctype="multipart/form-data">
+            <input hidden type="text" name="type" value="passchange"></input>
+            Password:<input type="text" name="newpwd" value="{getpass()}" required></input>
+            <input type="submit" value="Change Password"></input>
+            </form>
+"""
+    elif "usrpref" in request.args.keys():#user prefrences
+        #like dark mode vs light mode type shit
+        pref=getpref()
+        if request.method == "POST":
+            print(request.form)
+        if pref:
+            themeDescriptor="Default" if pref['theme'] == "light" else "Checked"
+            theme=pref['theme']
+            panel=f"""
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="form-check form-switch">
+                <input name="theme switch" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheck{themeDescriptor}">
+                <label class="form-check-label" for="flexSwitchCheck{themeDescriptor}">{theme}</label>
+            </div>
+            <input type="submit" value="Apply Changes"></input>
+        </form>
+"""
     name,Login=getCookie()
-    return render_template('about.html',login=Login,name=name)
+    return render_template('about.html',login=Login,name=name,panel=panel)
 # main driver function
 if __name__ == '__main__':
 
